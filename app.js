5e1d5a648637b2398119e4d87385bbfa37f78e64
tokov <flyingcatsad@gmail.com>
2022-12-22 14:15:34 +0200

    Import router

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const router = require("./routes");
 
 const app = express();
 const PORT = 5000;
@@ -6,6 +7,8 @@ const PORT = 5000;
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+app.use("/api", router);
 
 app.listen(PORT, () => {
   console.log(`App is listening on port ${PORT}`);

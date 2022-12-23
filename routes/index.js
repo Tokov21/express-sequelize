37457dc0c87bd109f668afc29e7951aa49c9e246
tokov <flyingcatsad@gmail.com>
2022-12-23 17:31:40 +0200

    Change router

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
-const userRouter = require("./users.router");
+const ordersRouter = require("./orders.router");
+const usersRouter = require("./users.router");
 
 router.get("/", (req, res) => {
   res.send("router");
 });
 
-router.use("/users", userRouter);
+router.use("/users", usersRouter);
+router.use("/orders", ordersRouter);
 
 module.exports = router;
